59536901200699dd7e82c4bf2e459b895e713415
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2019-11-09 12:26:09 -0500

    add IGameSide. 	- And add that to IUnitOnMap. Units have game sides!

---
+++ ProjectArchipelago/IGameSide.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ProjectArchipelago
+{
+    /// <summary>
+    /// All positions on the same Game Side win and
+    /// lose together. Probably.
+    /// </summary>
+    public interface IGameSide
+    {
+        /// <summary>
+        /// The Game Side's name.
+        /// </summary>
+        /// <returns>Game Side's name</returns>
+        string GetName();
+        /// <summary>
+        /// The Game Side's name.
+        /// </summary>
+        /// <param name="value"></param>
+        void SetName(string value);
+    }
+}
--- ProjectArchipelago/IUnitOnMap.cs
+++ ProjectArchipelago/IUnitOnMap.cs
@@ -4,6 +4,15 @@ namespace ProjectArchipelago
 {
     public interface IUnitOnMap
     {
-
+        /// <summary>
+        /// Unit's Game Side
+        /// </summary>
+        /// <returns>Unit's Game Side</returns>
+        IGameSide GetMyGameSide();
+        /// <summary>
+        /// Unit's Game Side
+        /// </summary>
+        /// <param name="value"></param>
+        void SetMyGameSide(IGameSide value);
     }
 }
