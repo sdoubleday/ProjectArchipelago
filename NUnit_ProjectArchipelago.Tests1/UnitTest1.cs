3408e3a7feb3c156654484cb3cdaf3d069905e70
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2019-11-09 11:52:36 -0500

    Create project and NUnit project. 	- Using .NET Core 3.0 to take advantage of trimmed self contained deployment. 	- It isn't until you sit down to create a class for the basic unit that you realize how many other classes you will need first.

---
+++ NUnit_ProjectArchipelago.Tests1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NUnit_ProjectArchipelago.Tests1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ ProjectArchipelago/IUnitOnMap.cs
@@ -0,0 +1,9 @@
+﻿    using System;
+
+namespace ProjectArchipelago
+{
+    public interface IUnitOnMap
+    {
+
+    }
+}
